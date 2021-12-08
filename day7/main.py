090b11aecf664f0501a3a2d85c47f41db805b1af
Vivian Ding <vyd2@cornell.edu>
2021-12-08 01:14:11 -0500

    wtf

--- day7/main.py
+++ day7/main.py
@@ -3,22 +3,20 @@ with open('input.txt', 'r') as f:
 
 
 def p1():
-    fuel_const = 0
+    fuel = 0
     crabs.sort()
-    position = crabs[round(len(crabs) / 2)]
+    position = crabs[round(len(crabs) / 2)]  # median
     for crab in crabs:
-        fuel_const += abs(position - crab)
-    print(fuel_const)
+        fuel += abs(position - crab)
+    print(fuel)
 
 
 def p2():
     fuel = 0
-    position = round(sum(crabs) / len(crabs))  # for the example, rounded up. for actual, rounded down. not sure why
+    position = round(sum(crabs) / len(crabs))  # average. for sample, rounded up. for actual, rounded down. not sure why
     for crab in crabs:
-        fl = 0
         for dist in range(abs(position - crab)):
             fuel += dist + 1
-        fuel += fl
     print(fuel)
 
 
